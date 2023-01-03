12a8eb3ebdc55183d75788d8c69af390309dccaf
sungeonlee <devcez82@gmail.com>
2023-01-03 10:13:26 +0900

    Unit 1.3/1.4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float speed = 20.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float forwardInput;
+    private float horizontalInput;
 
     private void Update()
     {
         // Move the vehicle forward
-        //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        /*transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);*/
+
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Moves the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
+
+        // Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
